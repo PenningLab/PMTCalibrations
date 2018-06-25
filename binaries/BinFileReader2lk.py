1e1bb09aae662bb18161247987a225e06d25b0a4
lkorley <lukekorley@gmail.com>
2018-06-25 15:51:01 -0400

    Changes to converter file

--- binaries/BinFileReader2lk.py
+++ binaries/BinFileReader2lk.py
@@ -36,52 +36,7 @@ def ReadBinFilelk(filename,number_of_channels,event):
 		waveform_array[j] = binary_file[current_index_location:current_index_location+length_per_waveform]
 	return waveform_array    
 
-def ReadBinFile(filename,number_of_channels):
-	f = open(filename, "r")
-	binary_file = np.fromfile(f, dtype=np.int16)
-	bit1 = binary_file[0]
-	bit2 = binary_file[1]
-	
-	if np.logical_and(bit2==0,bit1>0):
-		number_of_events = bit1
-	elif np.logical_and(bit2==1,bit1<0):
-		number_of_events = 2**17+bit1
-	else:
-		number_of_events = 2**16+bit1
-	print(number_of_events)
-	#number_of_events = binary_file[0]
-	length_per_waveform = binary_file[2]
-	start_skip = 12
-	deadspace_between_channels = 6
-	waveform_array = np.zeros(shape=(number_of_channels,number_of_events,length_per_waveform),dtype=np.int16)
-	one_event_length = number_of_channels*(length_per_waveform+deadspace_between_channels)
-	for i in range(number_of_events):
-		print("event "+str(i))
-		for j in range(number_of_channels):
-			current_index_location = start_skip +i*(one_event_length)+ j*(length_per_waveform+ deadspace_between_channels)
-			waveform_array[j,i] = binary_file[current_index_location:current_index_location+length_per_waveform]
-	return waveform_array,number_of_events
-
-def ReadBinFilelk2(filename,channels,evt):
-	Nevts, samples = ReadBinFileHeader(filename)
-	f = open(filename,"r")
-	ba = bytearray(f.read())
-	print(len(ba))
-	samples = samples
-	waveforms = np.zeros(shape=(channels,samples),dtype=np.int16)
-	skip = 24
-	deadspace = 6
-	for j in range(channels):
-		indexlow = skip + 2*evt*channels*(samples+deadspace) + 2*j*(samples+deadspace)
-		for k in range(samples):
-			indexlowk = indexlow +2*k
-			thispoint = ba[indexlow:(indexlow + 2)]
-			waveforms[j][k] = int.from_bytes(thispoint, byteorder='big',signed=True)
-	return waveforms
-
-
-
-def B2T(filename,number_of_channels,channel):
+def B2T_leg(filename,number_of_channels,channel):
 	f = open(filename, "r")
 	binary_file = np.fromfile(f, dtype=np.int16)
 	bit1 = binary_file[0]
@@ -101,6 +56,7 @@ def B2T(filename,number_of_channels,channel):
 	#binary_file = binary_file[6:]
 	#binary_file = binary_file.tobytes()
 	#binary_file = np.fromstring(binary_file,dtype=np.int16)
+	lend = "\n"
 	s = filename.rstrip(".bin")
 	s += ".txt"
 	fout = open(s,"w")
@@ -115,3 +71,50 @@ def B2T(filename,number_of_channels,channel):
 			fout.write(s)
 	fout.close()
 	   
+
+def Read_DDC10_BinWaveCap_ChSel(fName):
+    #fName = "test.bin"
+    waveInfo = {}
+    
+    fp = open(fName,"rb")
+    
+    numEvents = int(np.fromfile(fp,dtype=np.uint32,count=1))
+    waveInfo['numEvents'] = numEvents
+    numSamples = int(np.fromfile(fp,dtype=np.uint32,count=1))
+    waveInfo['numSamples'] = numSamples
+    chSelMask = int(np.fromfile(fp,dtype=np.uint32,count=1))
+    chMap = np.where(bitfield(chSelMask))[0]
+    waveInfo['chMap'] = chMap
+    numChannels = len(chMap)
+    waveInfo['numChannels'] = numChannels
+    byteOrderPattern = hex(int(np.fromfile(fp,dtype=np.uint32,count=1)))
+    
+    waveArr = np.zeros((numChannels,numEvents,numSamples),dtype=np.int16)
+    
+    for ievt in range(numEvents):
+        for ich in range(numChannels):
+            dummy = np.fromfile(fp,dtype=np.uint32,count=2)
+            waveTmp = np.fromfile(fp,dtype=np.int16,count=numSamples)
+            waveArr[ich,ievt,:] = waveTmp
+            dummy = np.fromfile(fp,dtype=np.uint32,count=1)
+    fp.close()
+    return (waveArr,waveInfo)
+
+
+def B2T(filename,channel):
+	waveform,waveInfo = Read_DDC10_BinWaveCap_ChSel(filename)
+	if(channel>=len(waveInfo['chMap'])):
+		print("Cannot find channel. Check user supplied index")
+		return
+
+	s = filename.rstrip(".bin")
+	s += ".txt"
+	lend = "\n"
+	fout = open(s,"w")
+	for i in range(waveInfo['numEvents']):
+		fout.write("Event "+str(i)+str(lend))
+		for j in range(waveInfo['numSamples']):
+			s = str(waveform[channel,i,j])+lend
+			fout.write(s)
+	fout.close()
+
